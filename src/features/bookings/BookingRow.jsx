ecfd9df7a6d2e605b748a9426910d84d49a1f10d
bagdenius <bagdensparrow@gmail.com>
2025-08-18 00:14:00 +0300

    build the single booking page

--- src/features/bookings/BookingRow.jsx
+++ src/features/bookings/BookingRow.jsx
@@ -1,11 +1,13 @@
 import styled from 'styled-components';
 import { format, isToday } from 'date-fns';
+import { HiEllipsisVertical, HiEye } from 'react-icons/hi2';
 
 import Tag from '../../ui/Tag';
 import Table from '../../ui/Table';
+import Menus from '../../ui/Menus';
+import { useNavigate } from 'react-router-dom';
 
-import { formatCurrency } from '../../utils/helpers';
-import { formatDistanceFromNow } from '../../utils/helpers';
+import { formatCurrency, formatDistanceFromNow } from '../../utils/helpers';
 
 const Cabin = styled.div`
   font-size: 1.6rem;
@@ -48,6 +50,8 @@ function BookingRow({
     cabins: { name: cabinName },
   },
 }) {
+  const navigate = useNavigate();
+
   const statusToTagName = {
     unconfirmed: 'blue',
     'checked-in': 'green',
@@ -79,6 +83,20 @@ function BookingRow({
       <Tag type={statusToTagName[status]}>{status.replace('-', ' ')}</Tag>
 
       <Amount>{formatCurrency(totalPrice)}</Amount>
+
+      <Menus.Menu>
+        <Menus.Toggle id={bookingId}>
+          <HiEllipsisVertical />
+        </Menus.Toggle>
+        <Menus.List id={bookingId}>
+          <Menus.Button
+            icon={<HiEye />}
+            onClick={() => navigate(`/bookings/${bookingId}`)}
+          >
+            See details
+          </Menus.Button>
+        </Menus.List>
+      </Menus.Menu>
     </Table.Row>
   );
 }
---
+++ src/features/bookings/useFetchBooking.js
@@ -0,0 +1,19 @@
+import { useQuery } from '@tanstack/react-query';
+import { useParams } from 'react-router-dom';
+import { getBooking } from '../../services/apiBookings';
+
+export function useFetchBooking() {
+  const { bookingId } = useParams();
+
+  const {
+    data: booking,
+    isPending: isLoading,
+    error,
+  } = useQuery({
+    queryKey: ['bookings'],
+    queryFn: () => getBooking(bookingId),
+    retry: false,
+  });
+
+  return { booking, isLoading, error };
+}
