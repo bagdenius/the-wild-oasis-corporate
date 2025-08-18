05dd41f3f2017ccf28951d4534541a9bd39f0efc
bagdenius <bagdensparrow@gmail.com>
2025-08-18 23:46:10 +0300

    implement user sign up

--- src/features/authentication/SignupForm.jsx
+++ src/features/authentication/SignupForm.jsx
@@ -1,16 +1,19 @@
 import { useForm } from 'react-hook-form';
+
 import Button from '../../ui/Button';
 import Form from '../../ui/Form';
 import FormRow from '../../ui/FormRow';
 import Input from '../../ui/Input';
-
-// Email regex: /\S+@\S+\.\S+/
+import { useSignup } from './useSignup';
 
 function SignupForm() {
-  const { register, formState, getValues, handleSubmit } = useForm();
+  const { signup, isLoading } = useSignup();
+  const { register, formState, getValues, handleSubmit, reset } = useForm();
   const { errors } = formState;
 
-  function onSubmit(data) {}
+  function onSubmit({ fullName, email, password }) {
+    signup({ fullName, email, password }, { onSettled: reset });
+  }
 
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
@@ -18,6 +21,7 @@ function SignupForm() {
         <Input
           type='text'
           id='fullName'
+          disabled={isLoading}
           {...register('fullName', { required: 'This field is required' })}
         />
       </FormRow>
@@ -26,6 +30,7 @@ function SignupForm() {
         <Input
           type='email'
           id='email'
+          disabled={isLoading}
           {...register('email', {
             required: 'This field is required',
             pattern: {
@@ -43,6 +48,7 @@ function SignupForm() {
         <Input
           type='password'
           id='password'
+          disabled={isLoading}
           {...register('password', {
             required: 'This field is required',
             minLength: {
@@ -57,6 +63,7 @@ function SignupForm() {
         <Input
           type='password'
           id='passwordConfirm'
+          disabled={isLoading}
           {...register('passwordConfirm', {
             required: 'This field is required',
             validate: (value) =>
@@ -67,10 +74,10 @@ function SignupForm() {
 
       <FormRow>
         {/* type is an HTML attribute! */}
-        <Button variation='secondary' type='reset'>
+        <Button $variation='secondary' type='reset' disabled={isLoading}>
           Cancel
         </Button>
-        <Button>Create new user</Button>
+        <Button disabled={isLoading}>Create new user</Button>
       </FormRow>
     </Form>
   );
