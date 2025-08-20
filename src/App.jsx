b98017bc72e8c512fcf21cbe24925aa1dee3bc34
bagdenius <bagdensparrow@gmail.com>
2025-08-20 13:12:48 +0300

    update vercel.js and some manage of app imports

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ import Checkin from './pages/Checkin';
 
 import AppLayout from './ui/AppLayout';
 import ProtectedRoute from './ui/ProtectedRoute';
+
 import { DarkModeProvider } from './context/DarkModeContext';
 
 const queryClient = new QueryClient({
