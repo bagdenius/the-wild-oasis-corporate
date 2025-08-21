a5c347015dfd5731d3982b4b8ded9b4a8099ec2d
bagdenius <bagdensparrow@gmail.com>
2025-08-22 01:34:42 +0300

    change public supabase key to publishable

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabaseUrl = 'https://hpdzplrfbxebonqqcvdc.supabase.co';
-const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhwZHpwbHJmYnhlYm9ucXFjdmRjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQwODM5MzcsImV4cCI6MjA2OTY1OTkzN30.royVzo3IfhhjFK6EPpm_3Pu7elO69EmhJYHbBKTjpSg';
+const supabaseKey = 'sb_publishable_2u4ljJMdy3a4aWSPnaugeg_CXYdO2ak';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
